49dfae1cb324cb82479c79c7b8e9b2701f4f6cfa
Majeed-hub <majeedbkl65@gmail.com>
2023-06-09 20:28:11 +0530

    offloaded from device

---
+++ home1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<?php include 'admin/studentsidebar.php'; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home1.css">
+    <link rel="stylesheet" href="sidebar1.css">
+</head>
+
+<body>
+   
+
+    
+
+
+
+     
+    </div>
+    <div class="description">
+        <h2>HOST<span>el</span></h2>
+        <center>
+            <p>designed to manage all hostel activities like hostel admissions,<br>fees,room,mess allotment & generates
+                related reports for smooth transactions.</p>
+            <button type="button">Get Started &#8594;</button>
+        </center>
+    </div>
+    <div class="credit">
+        <a href='https://www.freepik.com/vectors/login'>Login vector created by storyset - www.freepik.com</a>
+    </div>
+
+    <div class="student">
+        <img src="43913.jpg">
+        <button type="button"><a href="admin/register.php">Student Register</a></button>
+    </div>
+    <div class="admin">
+        <img src="60111.jpg">
+        <button  type="button"><a href="admin/index.php">Admin Login</a></button>
+    </div>
+    <div class="query">
+<img src="queries.jpg">
+<button type="button"><a href="admin/querybox.php">Drop Queries</a></button>
+    </div>
